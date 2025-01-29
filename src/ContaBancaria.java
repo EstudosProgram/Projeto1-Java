24190b1857e63e985d0c9a056ffa7ad7c00465d5
Milena <milena.cod@gmail.com>
2025-01-29 10:13:03 -0300

    meupdate nu de opções

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -3,32 +3,29 @@ import java.util.Scanner;
 public class ContaBancaria {
 public static void main(String[] args) {
 
+    String nome = "Milena";
+    String conta = "Corrente";
     double saldoInicial = 1000;
     int escolhaOpcao;
 
     Scanner leitura  = new Scanner(System.in);
 
-    System.out.println(
-            """
-            ********************************
-            Dados iniciais do cliente:
-            
-            Nome:           Milena
-            Tipo conta:     Corrente
-            Saldo inicial:  R$ 1000,00
-            
-            ********************************
-            
-            
-            Operações
+    System.out.println("**************************************");
+    System.out.println("\nNome do cliente: " + nome);
+    System.out.println("Tipo de conta: " + conta);
+    System.out.println("Saldo atual: " + saldoInicial);
+    System.out.println("\n**************************************");
+
+    System.out.println(""" 
+            \nOperações
             
             1- Consultar saldo
             2- Receber valor
             3- Transferir valor
             4- Sair
             
-            Digite a opção desejada: """
-    );
+            Digite a opção desejada:""");
+
     escolhaOpcao = leitura.nextInt();
 
 
