6d1350ec2211293239e05081abc2dabed1716097
Milena <milena.cod@gmail.com>
2025-01-30 10:23:53 -0300

    lógica criada

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -5,18 +5,18 @@ public static void main(String[] args) {
 
     String nome = "Milena";
     String conta = "Corrente";
-    double saldoInicial = 1000;
-    int escolhaOpcao;
+    double saldo = 1000.00;
+    int escolhaOpcao = 0;
 
-    Scanner leitura  = new Scanner(System.in);
+    Scanner leitura = new Scanner(System.in);
 
     System.out.println("**************************************");
     System.out.println("\nNome do cliente: " + nome);
     System.out.println("Tipo de conta: " + conta);
-    System.out.println("Saldo atual: " + saldoInicial);
+    System.out.println("Saldo atual: " + saldo);
     System.out.println("\n**************************************");
 
-    System.out.println(""" 
+    String menu = """ 
             \nOperações
             
             1- Consultar saldo
@@ -24,11 +24,43 @@ public static void main(String[] args) {
             3- Transferir valor
             4- Sair
             
-            Digite a opção desejada:""");
-
-    escolhaOpcao = leitura.nextInt();
+            Digite a opção desejada:""";
 
 
+    while (escolhaOpcao != 4) {
+        System.out.println(menu);
+        escolhaOpcao = leitura.nextInt();
 
+        if (escolhaOpcao == 1) {
+
+            System.out.println("Seu saldo atual é: R$" + saldo);
+
+        } else if (escolhaOpcao == 2) {
+
+            System.out.println("Quanto é o valor que você vai receber?");
+            double valorRecebido = leitura.nextDouble();
+            saldo += valorRecebido;
+            System.out.println("Seu saldo atualizado: R$" + saldo);
+
+        } else if (escolhaOpcao == 3) {
+
+            System.out.println("Digite o valor que queira transferir:");
+            double valortransferido = leitura.nextDouble();
+
+            if (valortransferido > saldo) {
+                System.out.println("Você não tem saldo o suficiente para essa transferência");
+            } else {
+                saldo -= valortransferido;
+                System.out.println("saldo atualizado: R$" + saldo);
+            }
+
+        } else if (escolhaOpcao != 4){
+
+            System.out.println("Opção inválida!");
+
+        } else {
+            System.out.println("Fechando o banco...");
+        }
+    }
 }
 }
